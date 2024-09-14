c8812ad1afc1f37546492184131b5ea8ab85439c
Cao Van Kien <kienb2105578@student.ctu.edu.vn>
2024-09-14 19:39:21 +0700

    Cac file co dinh

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 
+const contactsRouter = require("./app/routes/contact.route");
+
+const ApiError = require("./app/api-error");
+
 const app = express();
 
 app.use(cors());
@@ -10,4 +14,20 @@ app.get("/", (req, res) =>  {
     res.json({ message: "Welcome to contact book application."});
 });
 
+app.use("/api/contacts", contactsRouter);
+
+app.use ((req, res, next) => {
+    //Code ở đay sẽ chạy khi không có route được định nghĩa nào
+    //khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+    return next(new ApiError(404, "Resource not found"));
+});
+
+//define error-handing middleware lát , after other app.use() and routes calls
+app.use((err, req, res, next) => {
+    //Middleware xử lý lỗi tập trung.
+    //Trong cac đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
+    return res.status(console.error.statusCode || 500).json({
+        message: err.message || "Internal Server Error",
+    });
+});
 module.exports = app;
