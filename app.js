72ca852d0ea034359964f23c64bc1965bc7de1e7
Cao Van Kien <kienb2105578@student.ctu.edu.vn>
2024-09-19 07:14:44 +0700

    Cap nhat lỗi nhỏ

--- app.js
+++ app.js
@@ -17,15 +17,10 @@ app.get("/", (req, res) =>  {
 app.use("/api/contacts", contactsRouter);
 
 app.use ((req, res, next) => {
-    //Code ở đay sẽ chạy khi không có route được định nghĩa nào
-    //khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
     return next(new ApiError(404, "Resource not found"));
 });
 
-//define error-handing middleware lát , after other app.use() and routes calls
 app.use((err, req, res, next) => {
-    //Middleware xử lý lỗi tập trung.
-    //Trong cac đoạn code xử lý ở các route, gọi next(error) sẽ chuyển về middleware xử lý lỗi này
     return res.status(console.error.statusCode || 500).json({
         message: err.message || "Internal Server Error",
     });
